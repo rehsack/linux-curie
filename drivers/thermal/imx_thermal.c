9b378f3be153cc5f7bd98b360ced9db9fe102920
Jens Rehsack <sno@netbsd.org>
2015-06-17 17:30:50 +0200

    Port several imx_temp functions from 3.14 and
    fix irq handling
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- drivers/thermal/imx_thermal.c
+++ drivers/thermal/imx_thermal.c
@@ -15,6 +15,7 @@
 #include <linux/device_cooling.h>
 #include <linux/fsl_otp.h>
 #include <linux/init.h>
+#include <linux/interrupt.h>
 #include <linux/io.h>
 #include <linux/kernel.h>
 #include <linux/mfd/syscon.h>
@@ -34,6 +35,8 @@
 #define MISC0_REFTOP_SELBIASOFF		(1 << 3)
 
 #define TEMPSENSE0			0x0180
+#define TEMPSENSE0_ALARM_VALUE_SHIFT	20
+#define TEMPSENSE0_ALARM_VALUE_MASK	(0xfff << TEMPSENSE0_ALARM_VALUE_SHIFT)
 #define TEMPSENSE0_TEMP_CNT_SHIFT	8
 #define TEMPSENSE0_TEMP_CNT_MASK	(0xfff << TEMPSENSE0_TEMP_CNT_SHIFT)
 #define TEMPSENSE0_FINISHED		(1 << 2)
@@ -56,8 +59,8 @@
  * It defines the temperature in millicelsius for passive trip point
  * that will trigger cooling action when crossed.
  */
-#define IMX_TEMP_MAX_PASSIVE		85000
-#define IMX_TEMP_MIN_TRIP_DELTA		6000
+#define IMX_TEMP_PASSIVE		85000
+#define IMX_TEMP_PASSIVE_COOL_DELTA		6000
 
 #define IMX_POLLING_DELAY		3000 /* millisecond */
 #define IMX_PASSIVE_DELAY		2000
@@ -66,14 +69,13 @@
 #define FACTOR1				15976
 #define FACTOR2				4297157
 
-#define IMX_TRIP_PASSIVE	0
+#define IMX_TRIP_PASSIVE		0
 
 struct imx_thermal_data {
 	struct thermal_zone_device *tz;
 	struct thermal_cooling_device *cdev[2];
 	enum thermal_device_mode mode;
 	struct regmap *tempmon;
-	unsigned long trip_temp[IMX_TRIP_NUM];
 	u32 c1, c2; /* See formula in imx_get_sensor_data() */
 	unsigned long temp_passive;
 	unsigned long temp_critical;
@@ -86,6 +88,19 @@ struct imx_thermal_data {
 	struct clk *thermal_clk;
 };
 
+static void imx_set_alarm_temp(struct imx_thermal_data *data,
+			       signed long alarm_temp)
+{
+	struct regmap *map = data->tempmon;
+	int alarm_value;
+
+	data->alarm_temp = alarm_temp;
+	alarm_value = (data->c2 - alarm_temp) / data->c1;
+	regmap_write(map, TEMPSENSE0 + REG_CLR, TEMPSENSE0_ALARM_VALUE_MASK);
+	regmap_write(map, TEMPSENSE0 + REG_SET, alarm_value <<
+			TEMPSENSE0_ALARM_VALUE_SHIFT);
+}
+
 static int imx_get_temp(struct thermal_zone_device *tz, unsigned long *temp)
 {
 	struct imx_thermal_data *data = tz->devdata;
@@ -93,7 +108,6 @@ static int imx_get_temp(struct thermal_zone_device *tz, unsigned long *temp)
 	static unsigned long last_temp;
 	unsigned int n_meas;
 	unsigned long cur_state;
-	bool wait;
 	u32 val;
 
 	clk_prepare_enable(data->thermal_clk);
@@ -198,8 +212,7 @@ static int imx_get_crit_temp(struct thermal_zone_device *tz,
 {
 	struct imx_thermal_data *data = tz->devdata;
 
-	*temp = data->trip_temp[IMX_TRIP_CRITICAL];
-
+	*temp = data->temp_critical;
 	return 0;
 }
 
@@ -207,9 +220,10 @@ static int imx_get_trip_temp(struct thermal_zone_device *tz, int trip,
 			     unsigned long *temp)
 {
 	struct imx_thermal_data *data = tz->devdata;
+	unsigned long temp_passive_delta = (data->temp_critical - data->temp_passive) / data->num_passive_trips;
 
 	*temp = (trip < data->num_passive_trips) ?
-			data->temp_passive + (trip * data->temp_zone_delta) :
+			data->temp_passive + (trip * temp_passive_delta) :
 			data->temp_critical;
 	return 0;
 }
@@ -222,16 +236,16 @@ static int imx_set_trip_temp(struct thermal_zone_device *tz, int trip,
 	if (trip > IMX_TRIP_PASSIVE)
 		return -EPERM;
 
-	if (trip == IMX_TRIP_PASSIVE && temp > IMX_TEMP_MAX_PASSIVE)
+	if (trip == IMX_TRIP_PASSIVE && temp > IMX_TEMP_PASSIVE)
 		return -EINVAL;
 
 	data->temp_passive = temp;
-	data->temp_zone_delta = (data->temp_critical - data->temp_passive) / data->num_passive_trips;
 
 	imx_set_alarm_temp(data, temp);
 
 	return 0;
 }
+
 
 static int imx_bind(struct thermal_zone_device *tz,
 		    struct thermal_cooling_device *cdev)
@@ -385,11 +399,11 @@ static void imx_set_thermal_defaults(struct imx_thermal_data *data)
 		 * Set the default passive cooling trip point,
 		 * can be changed from userspace.
 		 */
-		data->temp_passive = IMX_TEMP_MAX_PASSIVE;
+		data->temp_passive = IMX_TEMP_PASSIVE;
 
 		/*
 		 * The maximum die temperature set to 20 C higher than
-		 * IMX_TEMP_MAX_PASSIVE.
+		 * IMX_TEMP_PASSIVE.
 		 */
 		data->temp_critical = 1000 * 20 + data->temp_passive;
 		data->temp_zone_delta = (data->temp_critical - data->temp_passive) / data->num_passive_trips;
@@ -410,8 +424,8 @@ static void imx_set_thermal_defaults(struct imx_thermal_data *data)
 			data->temp_critical = 95000;
 			break;
 		}
-		data->temp_passive = data->temp_critical - (IMX_TEMP_MIN_TRIP_DELTA * data->num_passive_trips);
-		data->temp_zone_delta = IMX_TEMP_MIN_TRIP_DELTA;
+		data->temp_passive = data->temp_critical - (IMX_TEMP_PASSIVE_COOL_DELTA * data->num_passive_trips);
+		data->temp_zone_delta = IMX_TEMP_PASSIVE_COOL_DELTA;
 	}
 
 	pr_debug("THERMAL DEFAULTS: passive: %lu \
@@ -421,11 +435,34 @@ static void imx_set_thermal_defaults(struct imx_thermal_data *data)
 		 data->num_passive_trips, data->temp_zone_delta);
 }
 
+static irqreturn_t imx_thermal_alarm_irq(int irq, void *dev)
+{
+	struct imx_thermal_data *data = dev;
+
+	disable_irq_nosync(irq);
+	data->irq_enabled = false;
+
+	return IRQ_WAKE_THREAD;
+}
+
+static irqreturn_t imx_thermal_alarm_irq_thread(int irq, void *dev)
+{
+	struct imx_thermal_data *data = dev;
+
+	dev_dbg(&data->tz->device, "THERMAL ALARM: T > %lu\n",
+		data->alarm_temp / 1000);
+
+	thermal_zone_device_update(data->tz);
+
+	return IRQ_HANDLED;
+}
+
 static int imx_thermal_probe(struct platform_device *pdev)
 {
 	struct imx_thermal_data *data;
 	struct cpumask clip_cpus;
 	struct regmap *map;
+	int measure_freq;
 	int ret;
 
 	if (!cpufreq_get_current_driver()) {
